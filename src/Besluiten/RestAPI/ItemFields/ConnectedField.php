d09fe29f80d4a347284c71d3ffb4220fe7eb4f01
Mike van den Hoek <mike@yard.nl>
2024-12-06 17:11:48 +0100

    (fix): connected besluiten query in ConnectedField retrieves expired items

--- src/Besluiten/RestAPI/ItemFields/ConnectedField.php
+++ src/Besluiten/RestAPI/ItemFields/ConnectedField.php
@@ -11,94 +11,105 @@ use WP_Query;
 
 class ConnectedField extends CreatesFields
 {
-    /**
-     * @var Plugin
-     */
-    protected $plugin;
+	protected Plugin $plugin;
+	protected Besluit $respository;
 
-    public function __construct(Plugin $plugin)
-    {
-        $this->plugin      = $plugin;
-        $this->respository = new Besluit($plugin);
-    }
+	public function __construct(Plugin $plugin)
+	{
+		$this->plugin = $plugin;
+		$this->respository = new Besluit($plugin);
+	}
 
-    /**
-     * Creates an array of connected posts.
-     */
-    public function create(WP_Post $post): array
-    {
-        $besluit       = BesluitModel::makeFrom($post);
-        $categoriesIDs = $this->getCategoriesIDs($besluit);
+	/**
+	 * Creates an array of connected posts.
+	 */
+	public function create(WP_Post $post): array
+	{
+		$besluit = BesluitModel::makeFrom($post);
+		$categoriesIDs = $this->getCategoriesIDs($besluit);
 
-        return $this->getConnectedItems($categoriesIDs, $besluit->getID());
-    }
+		return $this->getConnectedItems($categoriesIDs, $besluit->getID());
+	}
 
-    protected function getCategoriesIDs(BesluitModel $besluit): array
-    {
-        $terms = $besluit->getTerms('public-decision-category');
+	protected function getCategoriesIDs(BesluitModel $besluit): array
+	{
+		$terms = $besluit->getTerms('public-decision-category');
 
-        if (!is_array($terms)) {
-            return [];
-        }
+		if (! is_array($terms)) {
+			return [];
+		}
 
-        return array_map(function ($term) {
-            return $term->term_id;
-        }, $terms);
-    }
+		return array_map(function ($term) {
+			return $term->term_id;
+		}, $terms);
+	}
 
-    /**
-     * Get connected items and exclude current post.
-     */
-    protected function getConnectedItems(array $categoriesIDs, int $besluitID): array
-    {
-        $items = $this->query($categoriesIDs, $besluitID);
+	/**
+	 * Get connected items and exclude current post.
+	 */
+	protected function getConnectedItems(array $categoriesIDs, int $besluitID): array
+	{
+		$items = $this->query($categoriesIDs, $besluitID);
 
-        if (empty($items)) {
-            return [];
-        }
+		if (empty($items)) {
+			return [];
+		}
 
-        return array_map(function (WP_Post $post) {
-            $besluit = BesluitModel::makeFrom($post);
+		return array_map(function (WP_Post $post) {
+			$besluit = BesluitModel::makeFrom($post);
 
-            return [
-                'id'                => $besluit->getID(),
-                'date'              => $besluit->getDateI18n('Y-m-d H:i:s'),
-                'portal_url'        => $this->respository->makePortalURL($besluit->getPostName()),
-                'title'             => $besluit->getTitle(),
-                'image'             => $besluit->getThumbnail(),
-                'content'           => $besluit->getContent(),
-                'excerpt'           => $besluit->getExcerpt(),
-                'slug'              => $besluit->getPostName(),
-                'type'              => $besluit->getPostType(),
-                'expired'           => (new ExpiredField($this->plugin))->create($post)
-            ];
-        }, $items);
-    }
+			return [
+				'id' => $besluit->getID(),
+				'date' => $besluit->getDateI18n('Y-m-d H:i:s'),
+				'portal_url' => $this->respository->makePortalURL($besluit->getPostName()),
+				'title' => $besluit->getTitle(),
+				'image' => $besluit->getThumbnail(),
+				'content' => $besluit->getContent(),
+				'excerpt' => $besluit->getExcerpt(),
+				'slug' => $besluit->getPostName(),
+				'type' => $besluit->getPostType(),
+				'expired' => (new ExpiredField($this->plugin))->create($post),
+			];
+		}, $items);
+	}
 
-    /**
-     * Get connected items based on taxonomy.
-     *
-     * @param array $categoriesIDs
-     * @param integer $besluitID
-     *
-     * @return array
-     */
-    protected function query(array $categoriesIDs, int $besluitID): array
-    {
-        $args = [
-            'post_type' => 'public-decision',
-            'tax_query' => [
-                [
-                    'taxonomy' => 'public-decision-category',
-                    'field'    => 'term_id',
-                    'terms'    => $categoriesIDs,
-                ]
-            ],
-            'post__not_in' => [$besluitID]
-        ];
+	/**
+	 * Get connected items based on taxonomy.
+	 *
+	 * @param array $categoriesIDs
+	 * @param int $besluitID
+	 *
+	 * @return array
+	 */
+	protected function query(array $categoriesIDs, int $besluitID): array
+	{
+		$args = [
+			'post_type' => 'public-decision',
+			'tax_query' => [
+				[
+					'taxonomy' => 'public-decision-category',
+					'field' => 'term_id',
+					'terms' => $categoriesIDs,
+				],
+			],
+			'meta_query' => [
+				'relation' => 'OR',
+				[
+					'key' => '_owc_public_decisions_expiration_date',
+					'value' => date('Y-m-d H:i:s'),
+					'compare' => '>',
+					'type' => 'DATE',
+				],
+				[
+					'key' => '_owc_public_decisions_expiration_date',
+					'compare' => 'NOT EXISTS',
+				],
+			],
+			'post__not_in' => [$besluitID],
+		];
 
-        $query = new WP_Query($args);
+		$query = new WP_Query($args);
 
-        return $query->posts;
-    }
+		return $query->posts;
+	}
 }
--- src/Besluiten/Support/CreatesFields.php
+++ src/Besluiten/Support/CreatesFields.php
@@ -8,60 +8,58 @@ use WP_Post;
 
 abstract class CreatesFields
 {
-    /**
-     * Instance of the Plugin.
-     *
-     * @var Plugin
-     */
-    protected $plugin;
+	/**
+	 * Instance of the Plugin.
+	 */
+	protected Plugin $plugin;
 
-    /** @var Closure */
-    protected $condition = null;
+	/** @var Closure */
+	protected $condition = null;
 
-    /**
-     * @param Plugin $plugin
-     */
-    public function __construct(Plugin $plugin)
-    {
-        $this->plugin = $plugin;
-    }
+	/**
+	 * @param Plugin $plugin
+	 */
+	public function __construct(Plugin $plugin)
+	{
+		$this->plugin = $plugin;
+	}
 
-    /**
-     * The default condition is true, can be overriden.
-     *
-     * @return callable
-     */
-    protected function condition(): callable
-    {
-        return function () {
-            return \__return_true();
-        };
-    }
+	/**
+	 * The default condition is true, can be overriden.
+	 *
+	 * @return callable
+	 */
+	protected function condition(): callable
+	{
+		return function () {
+			return \__return_true();
+		};
+	}
 
-    /**
-     * Run the current condition.
-     *
-     * @return null|Closure
-     */
-    public function executeCondition(): ?Closure
-    {
-        if ($this->condition() instanceof Closure) {
-            return $this->condition();
-        }
+	/**
+	 * Run the current condition.
+	 *
+	 * @return null|Closure
+	 */
+	public function executeCondition(): ?Closure
+	{
+		if ($this->condition() instanceof Closure) {
+			return $this->condition();
+		}
 
-        if (\is_callable($this->condition())) {
-            return $this->condition();
-        }
+		if (\is_callable($this->condition())) {
+			return $this->condition();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    /**
-     * Create an additional field on an array.
-     *
-     * @param WP_Post $post
-     *
-     * @return mixed
-     */
-    abstract public function create(WP_Post $post);
+	/**
+	 * Create an additional field on an array.
+	 *
+	 * @param WP_Post $post
+	 *
+	 * @return mixed
+	 */
+	abstract public function create(WP_Post $post);
 }
